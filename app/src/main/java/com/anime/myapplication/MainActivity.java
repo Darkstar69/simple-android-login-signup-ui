b59c8ddc78d8ba7fa0fd4637f7d95e3e61c87c1d
Arnab <thisisdarkstar@duck.com>
2024-03-25 19:52:26 +0530

    initial commit

---
+++ app/src/main/java/com/anime/myapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.anime.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    Intent intent;
+    public void goToSignup(View view) {
+        intent = new Intent(this, SignupPage.class);
+        startActivity(intent);
+    }
+}
